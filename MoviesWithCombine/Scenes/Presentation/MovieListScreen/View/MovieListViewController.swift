1253cf20bafefac59044be94bb4846c43672290a
waleed saad <Waleed_saad36@yahoo.com>
2023-02-21 21:37:31 +0200

    handle DataSource And Delegate for CollectionView

--- MoviesWithCombine/Scenes/Presentation/MovieListScreen/View/MovieListViewController.swift
+++ MoviesWithCombine/Scenes/Presentation/MovieListScreen/View/MovieListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MovieListViewController: UIViewController {
+class MovieListViewController: BaseViewController {
     
     //MARK:- Outlets
     @IBOutlet weak var MovieCollectionView: UICollectionView!
@@ -16,6 +16,7 @@ class MovieListViewController: UIViewController {
     weak var coordinator: AppCoordinator?
     
     //MARK:- Private Properties
+    private var viewmodel = MovieListViewModel()
     private lazy var MovieCustomCollectionViewLayout: PhotosCollectionViewCustomLayout = {
         let layout = PhotosCollectionViewCustomLayout(
             delegate: self,
@@ -29,6 +30,9 @@ class MovieListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
+        setTableViewCellConfigration()
+        subscribeMovieListChanges()
+        self.viewmodel.loadMovieList()
     }
     
     //MARK:- Initializer
@@ -39,20 +43,49 @@ class MovieListViewController: UIViewController {
     }
     private func setTableViewCellConfigration() {
         self.MovieCollectionView.registerCellNib(MovieCell.self)
-//        self.MovieCollectionView.delegate = self
-//        self.MovieCollectionView.dataSource = self
+        self.MovieCollectionView.delegate = self
+        self.MovieCollectionView.dataSource = self
         self.MovieCollectionView.showsVerticalScrollIndicator = false
         self.MovieCollectionView.showsHorizontalScrollIndicator = false
     }
     
+    private func subscribeMovieListChanges(){
+        viewmodel.$movieList.sink { [weak self] _ in
+            DispatchQueue.main.async {
+                self?.MovieCollectionView.reloadData()
+            }
+        }.store(in: &cancellables)
+    }
     
     
+}
+extension MovieListViewController:UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        guard indexPath.row == viewmodel.movieList.count - 2 else {return}
+        viewmodel.loadMoreMovieList()
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+extension MovieListViewController:UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewmodel.movieList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell:MovieCell = collectionView.dequeue(at:indexPath)
+        let isWideCell:Bool = viewmodel.isWideCell(for: indexPath)
+        let configerMovieCell =  cell.configer(movieData: viewmodel.getMovieData(for: indexPath))
+        let configerAdsCell =  cell.configerAds(data: viewmodel.getMovieData(for: indexPath))
+       
+        isWideCell ? configerAdsCell : configerMovieCell
+        return cell
+    }
 }
 
 extension MovieListViewController:CollectionViewCustomLayoutDelegate{
     func collectionView(_ collectionView: UICollectionView, sizeOfItemAtIndexPath indexPath: IndexPath) -> CGSize {
         return CGSize()
     }
-    
-    
 }
